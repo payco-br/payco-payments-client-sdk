9a269efc16acc6098524475d9c4a6c25c86bc19a
henrikato <henriquetmkato@gmail.com>
2024-08-20 16:37:59 -0300

    fix(repo): deleted dist folder from repo

--- dist/jest.config.d.ts
+++
@@ -1,3 +0,0 @@
-export let transform: {
-	"^.+\\.(t|j)sx?$": string;
-};
--- dist/src/functions/install-scripts.d.ts
+++
@@ -1,8 +0,0 @@
-export type InstallScriptsInput = {
-	orgId: string;
-	sessionId?: string;
-};
-export declare function installScripts({
-	orgId,
-	sessionId,
-}: InstallScriptsInput): string;
